343abde7c5b167893b3a34d6045d257223e39e22
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-05-05 17:15:30 +0200

    Initial commit
    ADD: Temporary tilesets in tilemap
    ADD: Level chunk spawning system
    ADD: 2 Temporary test chunks

---
+++ Assets/Scripts/LevelChunk.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChunk : MonoBehaviour
+{
+	//Config
+	[SerializeField] public Transform spawnPoint;
+
+	//Cache
+	LevelSpawner levelSpawner;
+
+	private void Awake()
+	{
+		levelSpawner = FindObjectOfType<LevelSpawner>();
+	}
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "SpawnCollider")
+		{
+			levelSpawner.SpawnChunk();
+		}
+		else if(other.gameObject.tag == "ShredCollider")
+		{
+			Destroy(transform.parent.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/LevelSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawner : MonoBehaviour
+{
+	//Config
+	[SerializeField] GameObject[] levelChunks;
+	[SerializeField] Transform chunkParent;
+	//[SerializeField] BoxCollider2D spawnCollider;
+	//[SerializeField] BoxCollider2D shredCollider;
+
+	//Cache
+	GameObject chunkToSpawn;
+	GameObject previousChunk;
+	GameObject currentChunk;
+
+	void Start()
+	{
+		SpawnFirstChunk();
+	}
+
+	private void SpawnFirstChunk()
+	{
+		GameObject firstChunk = levelChunks[Random.Range(0, levelChunks.Length)];
+		currentChunk = Instantiate(firstChunk, transform);
+		currentChunk.transform.parent = chunkParent;
+		previousChunk = currentChunk;
+	}
+
+	public void SpawnChunk()
+	{
+		chunkToSpawn = levelChunks[Random.Range(0, levelChunks.Length)];
+		currentChunk = Instantiate(chunkToSpawn, previousChunk.GetComponentInChildren<LevelChunk>().spawnPoint.transform);
+		currentChunk.transform.parent = chunkParent;
+		previousChunk = currentChunk;
+	}
+}
