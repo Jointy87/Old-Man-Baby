1c4d7f91673f92cc881a08e5622c40cc0e2424ff
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-05-06 10:46:52 +0200

    ADD: Prefabbed main camera ADD: Added follow cam that follows player

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+	//Config Parameters
+	[SerializeField] float lookAheadDistance;
+
+	//Cache
+	PlayerController player;
+	void Start()
+	{
+		player = FindObjectOfType<PlayerController>();
+	}
+
+
+	void Update()
+	{
+		FollowPlayer();
+	}
+
+	private void FollowPlayer()
+	{
+		Vector2 newCamPos = new Vector2(player.transform.position.x + lookAheadDistance, transform.position.y);
+		transform.position = new Vector3(newCamPos.x, newCamPos.y, transform.position.z);
+	}
+}
