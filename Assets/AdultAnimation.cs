80f3d0314882a8d86db94fda4e68d5bb993b26cc
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-05-09 14:46:34 +0200

    ADD: Adult animations. Attempted to have split upper and lower body but no succes yet. Will need to redo animations I think ADD: rest of baby animations

---
+++ Assets/AdultAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdultAnimation : MonoBehaviour
+{
+	[SerializeField] GameObject legs;
+	[SerializeField] GameObject torso;
+	[SerializeField] GameObject high5;
+
+	public void EnableHigh5()
+	{
+		high5.SetActive(true);
+		torso.SetActive(false);
+	}
+	public void DisableHigh5()
+	{
+		high5.SetActive(false);
+		torso.SetActive(true);
+	}
+
+}
